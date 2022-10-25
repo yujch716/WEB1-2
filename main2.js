f33559edc4f05315160b52efbb7f08465ddd9af8
yujch716 <yujch716@gmail.com>
2022-10-26 01:42:28 +0900

    callback

--- main2.js
+++ main2.js
@@ -38,4 +38,4 @@ var app = http.createServer(function(request,response){
  
  
 });
-app.listen(3000);
+app.listen(3001);
--- syntax/callback.js
+++ syntax/callback.js
@@ -1,15 +1,9 @@
-/*
-function a(){
-  console.log('A');
-}
-*/
-var a = function(){
+var a = function(){ //'a'라는 변수의 정의
     console.log('A');
-  }
+}
+
+function testfunc(callback){ //2.testfunc함수는 callback이라는 파라미터를 갖는다.
+    callback(); //3.callback 함수를 실행해라
+}
    
-   
-  function slowfunc(callback){
-    callback();
-  }
-   
-  slowfunc(a);
+testfunc(a); //1.testfunc함수를 변수a를 넣어서 실행해라
