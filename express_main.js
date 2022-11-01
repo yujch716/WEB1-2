ce33eccb701f0cd9422a49d0ed02991b9b3c73f2
yujch716 <yujch716@gmail.com>
2022-11-02 00:58:55 +0900

    20221102
    express

--- express_main.js
+++ express_main.js
@@ -1,25 +1,57 @@
-const { response } = require('express');
-const express = require('express');
-const fs = require('fs');
+// const { response } = require('express');
+const express = require('express')
+// const { fstat } = require('fs')
 const app = express()
+// var fs = require('fs');
+// var qs = required('querystring');
+// var sanitizeHtml = required('sanitize-html');
+// var template = reuqire('./lib/template.js');
 const port = 3003
 
 app.get('/', (req, res) => {
-    fstat.
-  var title = 'Welcome!';
-  var description = 'Hello Node.js';
-  var list = template.list(FileList);
-  var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description}`
-    `<a href="/create">create</a>`
-    );
-    response.writeHead(200);
-    response.end(html);
-});
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// app.get('/create', function(request, response){
+//     fs.readdir('./data', function(error, filelist){
+//       var title = 'WEB - create';
+//       var list = template.list(filelist);
+//       var html = template.HTML(title, list, `
+//         <form action="/create_process" method="post">
+//           <p><input type="text" name="title" placeholder="title"></p>
+//           <p>
+//             <textarea name="description" placeholder="description"></textarea>
+//           </p>
+//           <p>
+//             <input type="submit">
+//           </p>
+//         </form>
+//       `, '');
+//       response.send(html);
+//     });
+//   });
+   
+//   app.post('/create_process', function(request, response){
+//     var body = '';
+//     request.on('data', function(data){
+//         body = body + data;
+//     });
+//     request.on('end', function(){
+//         var post = qs.parse(body);
+//         var title = post.title;
+//         var description = post.description;
+//         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+//           response.writeHead(302, {Location: `/?id=${title}`});
+//           response.end();
+//         })
+//     });
+//   });
+
+
 
 /*
 var http = require('http');
