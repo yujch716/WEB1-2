e96a49bc51afe929959195059d53d02e8b2dcc26
yujch716 <yujch716@gmail.com>
2022-11-02 00:23:45 +0900

    20221102
    express

---
+++ express_main.js
@@ -0,0 +1,66 @@
+const { response } = require('express');
+const express = require('express');
+const fs = require('fs');
+const app = express()
+const port = 3003
+
+app.get('/', (req, res) => {
+    fstat.
+  var title = 'Welcome!';
+  var description = 'Hello Node.js';
+  var list = template.list(FileList);
+  var html = template.HTML(title, list,
+    `<h2>${title}</h2>${description}`
+    `<a href="/create">create</a>`
+    );
+    response.writeHead(200);
+    response.end(html);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+/*
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    if(_url == '/'){
+      title = 'Welcome';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.end(template);
+    })
+ 
+ 
+});
+app.listen(3001);
+*/
