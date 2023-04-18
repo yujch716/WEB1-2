f682e47f585f595da12cfe4f0fee889a08a172aa
stead-yujin <70509403+yujch716@users.noreply.github.com>
2023-04-19 03:18:49 +0900

    form

--- node_main2.js
+++ node_main2.js
@@ -40,7 +40,7 @@ var app = http.createServer(function(request,response){
       if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
-          var description = 'Hello, Node.js';
+          var description = 'Hello, WEB WEB';
           var list = templateList(filelist);
           var template = templateHTML(title, list, 
             `<h2>${title}</h2>${description}`,
@@ -56,7 +56,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `<a href="/create">create</a> 
+              <a href="/update?id=${title}">update</a>
+              <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
               );
             response.writeHead(200);
             response.end(template);
@@ -122,6 +127,38 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
+      });
+    
+    } else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          });
+      });
+    
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
       });
     
     } else {
